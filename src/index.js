ea34c4cf80dbeb7d484bfab30d457a4cf173ea7c
KatyaVysevka <katrina_v89@mail.ru>
2017-09-24 18:34:48 +0300

    write my solution about this task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var dept = [];
+  var chars = str.split('');
+  for (var i = 0; i < chars.length; i++) {
+    var char = chars[i];
+
+    if (isBracketsClose(char) && dept.length != 0) {
+      var d = dept.pop();
+      if (char != d) {
+        dept.push(d);
+      } else {
+        continue;
+      }
+    }
+
+    var closeSymbol = calcCloseBracketBySymbol(char);
+    if (closeSymbol != null) {
+      dept.push(closeSymbol);
+    } else {
+      // close bracket
+      if (dept.length == 0 || dept.pop() != char) {
+        return false;
+      }
+    }
+  }
+
+  return dept.length == 0;
+
+  function calcCloseBracketBySymbol(symbol) {
+    for (var q = 0; q < bracketsConfig.length; q++) {
+      var brackets = bracketsConfig[q];
+      if (symbol == brackets[0]) {
+        return brackets[1];
+      }
+    }
+
+    return null;
+  }
+
+  function isBracketsClose(symbol) {
+    for (var q = 0; q < bracketsConfig.length; q++) {
+      var brackets = bracketsConfig[q];
+      if (symbol == brackets[1]) {
+        return true;
+      }
+    }
+
+    return false;
+  }
 }
